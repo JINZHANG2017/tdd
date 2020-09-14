357b251016feab0b654312799cfeae2f32bc0258
JINZHANG2017 <zj574406254@hotmail.com>
2020-09-14 17:00:47 +0800

    [ZhangJin] feat: finish test for more than 30per continuous vowels transering

--- src/main/java/VowelsStringTransfer.java
+++ src/main/java/VowelsStringTransfer.java
@@ -6,6 +6,57 @@ public class VowelsStringTransfer {
         if("".equals(originString)){
             return "";
         }
-        return null;
+        String returnString=originString;
+        int[] continuousVowelsPosAndLen=getMostContinuousVowelsPosAndLen(originString);
+        while(continuousVowelsPosAndLen[1]>=2&&continuousVowelsPosAndLen[1]>returnString.length()*0.3){
+            returnString=insertSequence(returnString,continuousVowelsPosAndLen);
+            continuousVowelsPosAndLen=getMostContinuousVowelsPosAndLen(returnString);
+        }
+        return returnString;
+    }
+
+    private static String insertSequence(String originString, int[] continuousVowelsPosAndLen) {
+        StringBuilder sb=new StringBuilder();
+        sb.append(originString.substring(0,continuousVowelsPosAndLen[0]+1));
+        sb.append("mommy");
+        sb.append(originString.substring(continuousVowelsPosAndLen[0]+1,originString.length()));
+        return sb.toString();
+    }
+
+    private static int[] getMostContinuousVowelsPosAndLen(String originString) {
+        int pos=0;
+        int len=0;
+        int maxLen=0;
+        int maxPos=0;
+        char tempStoredChar=' ';
+        for(int i=0;i<originString.length();i++){
+            char currentChar=originString.charAt(i);
+            if(isVowel(currentChar)){
+                if(tempStoredChar==' '){
+                    pos=i;
+                }
+                len++;
+                tempStoredChar=currentChar;
+            }else{
+                if(maxLen<len){
+                    maxLen=len;
+                    maxPos=pos;
+                }
+                len=0;
+                tempStoredChar=' ';
+            }
+        }
+        int[] result=new int[2];
+        result[0]=maxPos;
+        result[1]=maxLen;
+        return result;
+    }
+
+    private static boolean isVowel(char c) {
+        String vowelString="aeiou";
+        if(vowelString.indexOf(c)>=0){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,18 +1,40 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MommifierTest {
     @Test
-    void should_return_null_for_nullString(){
-        String testStr=null;
-        String result=VowelsStringTransfer.transfer(testStr);
+    void should_return_null_for_nullString() {
+        String testStr = null;
+        String result = VowelsStringTransfer.transfer(testStr);
         assertEquals(null, result);
     }
 
     @Test
-    void should_return_emptyString_for_emptyString(){
-        String testStr="";
-        String result=VowelsStringTransfer.transfer(testStr);
-        assertEquals("",result);
+    void should_return_emptyString_for_emptyString() {
+        String testStr = "";
+        String result = VowelsStringTransfer.transfer(testStr);
+        assertEquals("", result);
+    }
+
+    @Test
+    void should_return_originalString_if_no_more_than_30per_continuous_vowels() {
+        String testStr = "abcdeef";
+        String result = VowelsStringTransfer.transfer(testStr);
+        assertEquals(testStr, result);
+    }
+
+    @Test
+    void should_return_transferedString_if_more_than_30per_continuous_vowels(){
+        String testStr = "acdeef";
+        String result = VowelsStringTransfer.transfer(testStr);
+        assertEquals("acdemommyef", result);
+    }
+
+    @Test
+    void should_return_recursively_transferedString_if_more_than_30per_continuous_vowels(){
+        String testStr = "acdeeeeeef";
+        String result = VowelsStringTransfer.transfer(testStr);
+        assertEquals("acdemommyemommyeeeef", result);
     }
 }
